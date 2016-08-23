b042a26a0c4dcb0740a010db7bcea86dc3071008
fleyhe <fleyhe0@gmail.com>
2016-08-23 11:23:29 -0300

    PIM 23 / 08 / 2016

---
+++ PIL.py
@@ -0,0 +1,12 @@
+from PIL import Image
+import numpy as np
+imagem1=Image.open('lena.pgm')
+(l,h)=imagem1.size
+print(l,h)
+out=Image.new(imagem1.mode, (l,h))
+for j in range(0, h):
+    for i in range(0, l):
+        out.putpixel((i,j),imagem1.getpixel((i,j)))
+        print(imagem1.getpixel((i,j)))
+out.show()
+out.save("outLenaShort.jpg","JPEG")
